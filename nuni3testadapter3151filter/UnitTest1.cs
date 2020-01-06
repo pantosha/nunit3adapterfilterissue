44782e8a92cf841e25d62aaa7b3b0745032d7397
Anton Paulovich <apaulovich@aras.com>
2020-01-06 20:36:03 +0300

    The filter argument does not reduce the number of executed tests
    Command:
    `dotnet test .\nuni3testadapterfilter.sln
    --filter:"FullyQualifiedName=abc"`
    
    Expected result:
    No one test is run
    
    Actual result:
    * No one test is run in the project that references NUnit3TestAdapter
    3.15.1
    * Test is run in the project that references NUnit3TestAdapter 3.16

---
+++ nuni3testadapter3151filter/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace nuni3testadapter3151filter
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
